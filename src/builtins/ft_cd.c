fbf249070e047fb3f30f0b48bea420fb3acadda1
oldvibe <bobies431@gmail.com>
2025-07-20 15:58:20 +0100

    all merge

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -1,2 +1,29 @@
 #include "../../include/minishell.h"
 
+void    cd(t_cmd *cmd)
+{
+    char *path;
+    char cwd[1024];
+    path = cmd->args[1];
+    if (getcwd(cwd, sizeof(cwd)))
+        printf("CURRENT IS : %s\n",cwd);
+    
+    if(!path)
+    {
+        path = getenv("HOME");
+    }
+
+    if(chdir(path) != 0)
+    {
+        printf("CHANGING DIRECTORY FAILED!!");
+        return;
+    }
+    // if (getcwd(cwd, sizeof(cwd)))
+    // {
+    //     printf("CURRENT IS : %s\n",cwd);
+    //     setenv("PWD", cwd, 1);
+
+    // }
+    
+
+}
--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -1,1 +1,54 @@
 #include "../../include/minishell.h"
+
+
+
+int check_flag(char *arg)
+{
+    int i;
+    i = 0;
+    if(arg[i] != '-')
+        return(0);
+    while (arg[i], arg[i] == '-')
+    {
+        while (arg[i + 1], arg[i + 1] == 'n')
+        {
+            i++;
+        }
+        if(arg[i + 1] != 'n')
+            return 0;
+        
+    }
+    return(1);
+    
+}
+
+void echo(t_cmd *cmds)
+{
+    int i;
+    int new_line;
+
+    i = 2;
+    new_line = 0;
+    if (ft_strcmp(cmds->args[0], "echo") != 0)
+        return;
+    if(check_flag(cmds->args[i]))
+    {
+        new_line = 1;
+        i++;
+    }
+    while (cmds->args[i])
+    {
+        printf("%s", cmds->args[i]);
+        if(cmds->args[i + 1])
+            printf(" ");
+        i++;
+    }
+    if(new_line == 0)
+        printf("\n");
+    
+    
+
+
+
+
+}
